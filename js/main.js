4dcd5b0d7fe6758dbe98bfc2ae9461de8bf741e0
BeainaT <tania.df@hotmail.it>
2022-05-19 15:35:54 +0200

    added text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const app = new Vue ({
+    el: "#main_container",
+    data: {
+        text: "Hello Vue Js"
+    }
+});
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
