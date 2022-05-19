ed8fd37986cc4557b15db484e3d1396457826131
BeainaT <tania.df@hotmail.it>
2022-05-19 16:58:46 +0200

    added click function

--- js/main.js
+++ js/main.js
@@ -6,9 +6,18 @@ const app = new Vue ({
     data: {
         text: "Hello Vue Js, nice to meet you",
         style: "title",
+        answer: "Are you sure?",
         icon: {
             img: "https://cdn.eshirt.it/wp-content/uploads/2018/05/thm_00001_emoticons_smile_felice.png",
             desc: "smile emoji"
+        }
+    },
+    methods: {
+        getAnswer() {
+            this.text = "Uhm, I wouldn't be so sure!",
+            this.style = "sad",
+            this.icon.img = "https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f628.png",
+            this.answer = "but don't give up!"
         }
     }
 });
